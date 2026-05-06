b5644707070d0cddbf1cef06d25360a1fa6a098f
JesseJohn7 <Jessejohn260@gmail.com>
2026-05-06 04:57:32 +0100

    services

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Experience from './components/Experience';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
-import Services from './components/Services'
 import Skills from './components/Skills';
 
 
