72165d8b4fdcfdb6abf86d06ee46651f498a11a7
JesseJohn7 <Jessejohn260@gmail.com>
2026-07-04 14:02:06 +0100

    hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
           transition={{ duration: 1.2, ease: "easeOut" }}
         >
           <h1>
-            100x Software
+       Software
           </h1>
           <h2>Developer.</h2>
           <p className="sub-text">
